690ae6e40a1668b942bc9d252695a3908e547af2
MichaelUzilevsky <michael.uzilevsky@gmail.com>
2022-10-14 21:39:31 +0300

    connectivity was added

--- app/src/main/java/com/example/chess/GameOptions.java
+++ app/src/main/java/com/example/chess/GameOptions.java
@@ -1,14 +1,20 @@
 package com.example.chess;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,15 +22,25 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
 
 public class GameOptions extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
+    private Dialog codeDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_options);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         SharedPreferences sharedPreferences = getSharedPreferences("User_Data", 0);
         drawerLayout = findViewById(R.id.draw_layout);
@@ -44,14 +60,13 @@ public class GameOptions extends AppCompatActivity implements NavigationView.OnN
 
         navigationView.setCheckedItem(R.id.nav_game);
 
-        String username_sp =sharedPreferences.getString("username", "");
-        String name_sp =sharedPreferences.getString("name", "");
-        if(username_sp.equals("") && name_sp.equals("")){
+        String username_sp = sharedPreferences.getString("username", "");
+        String name_sp = sharedPreferences.getString("name", "");
+        if (username_sp.equals("") && name_sp.equals("")) {
             menu.findItem(R.id.nav_profile).setVisible(false);
             menu.findItem(R.id.nav_login).setVisible(true);
             menu.findItem(R.id.nav_signup).setVisible(true);
-        }
-        else{
+        } else {
             menu.findItem(R.id.nav_profile).setVisible(true);
             menu.findItem(R.id.nav_login).setVisible(false);
             menu.findItem(R.id.nav_signup).setVisible(false);
@@ -97,4 +112,117 @@ public class GameOptions extends AppCompatActivity implements NavigationView.OnN
         return true;
     }
 
+    public void openDialog(View view) {
+        codeDialog = new Dialog(this);
+        codeDialog.setContentView(R.layout.online_game_dialog);
+        codeDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        codeDialog.setCancelable(true);
+        codeDialog.show();
+    }
+
+    public void start_new_game(View view) {
+        TextInputLayout input_code = codeDialog.findViewById(R.id.code);
+        String code = input_code.getEditText().getText().toString();
+
+        if (!code.equals("")) {
+            DatabaseReference available_codes = FirebaseDatabase.getInstance().getReference("Available Codes");
+            DatabaseReference games = FirebaseDatabase.getInstance().getReference("Games");
+
+            String randomValue = generateRandomCode() ;
+
+            available_codes.orderByKey().equalTo(code).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (snapshot.exists()) {
+
+                        //creates the game in the DB
+                        games.child(randomValue).child("Board").setValue("");
+                        games.child(randomValue).child("Chat").setValue("");
+
+                        //removes the game code
+                        available_codes.orderByKey().equalTo(code).addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                for (DataSnapshot data : snapshot.getChildren()) {
+                                    data.getRef().removeValue();
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+                        Intent game = new Intent(GameOptions.this, OnlineGame.class);
+                        game.putExtra("game_code", randomValue);
+                        startActivity(game);
+
+                    }
+                    else {
+                        available_codes.child(code).setValue(code);
+
+                        codeDialog = new Dialog(GameOptions.this);
+                        openWaitDialog();
+
+
+                        DatabaseReference codeRef = FirebaseDatabase.getInstance().getReference("Available Codes").child(code);
+                        codeRef.addValueEventListener(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                if (snapshot.getValue() == null) {
+                                    Intent game = new Intent(GameOptions.this, OnlineGame.class);
+                                    game.putExtra("game_code", randomValue);
+                                    startActivity(game);
+                                }
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        }
+
+    }
+
+    public String generateRandomCode() {
+        Random random = new Random();
+        String cap_letters = "ABCDEFGHIJKLMNOPKRSTOVWXYZ";
+        String low_letters = "abcdefghijklmnopqrstuvwxyz";
+        String numbers = "1234567890";
+        StringBuilder code = new StringBuilder().append("ChessGame");
+        int length = 16;
+        for (int i = 0; i < length; i++) {
+            switch (random.nextInt(3)) {
+                case 0:
+                    code.append(cap_letters.toCharArray()[new Random().nextInt(cap_letters.length())]);
+                    break;
+                case 1:
+                    code.append(low_letters.toCharArray()[new Random().nextInt(low_letters.length())]);
+                    break;
+                case 2:
+                    code.append(numbers.toCharArray()[new Random().nextInt(numbers.length())]);
+                    break;
+            }
+        }
+        return code.toString();
+    }
+
+    public void openWaitDialog() {
+        Dialog waitDialog = new Dialog(this);
+        waitDialog.setContentView(R.layout.waiting);
+        waitDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        waitDialog.setCancelable(true);
+        waitDialog.show();
+    }
 }
